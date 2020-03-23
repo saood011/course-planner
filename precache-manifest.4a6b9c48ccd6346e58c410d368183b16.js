9d369c151528d535be20b0fc710fc9363edc552e
root <saood011@yahoo.com>
2020-03-23 13:51:18 +0100

    Updates

---
+++ precache-manifest.4a6b9c48ccd6346e58c410d368183b16.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721edb10feb0b6140d0deee25a3bad99",
+    "url": "/course-planner/index.html"
+  },
+  {
+    "revision": "ea4e522e862a7cb4b3d5",
+    "url": "/course-planner/static/css/2.6696f92c.chunk.css"
+  },
+  {
+    "revision": "1e72af92f8ddf5195855",
+    "url": "/course-planner/static/css/main.df986c12.chunk.css"
+  },
+  {
+    "revision": "ea4e522e862a7cb4b3d5",
+    "url": "/course-planner/static/js/2.42a218d0.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/course-planner/static/js/2.42a218d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e72af92f8ddf5195855",
+    "url": "/course-planner/static/js/main.902ff910.chunk.js"
+  },
+  {
+    "revision": "fb2623e077ece18c1b65",
+    "url": "/course-planner/static/js/runtime-main.901cdf3e.js"
+  },
+  {
+    "revision": "764d6c87432f294f3ddee59ca2ce9f64",
+    "url": "/course-planner/static/media/english.764d6c87.jpg"
+  },
+  {
+    "revision": "03f377e98d22c41ef35084a15a4df28b",
+    "url": "/course-planner/static/media/german.03f377e9.jpg"
+  }
+]);
